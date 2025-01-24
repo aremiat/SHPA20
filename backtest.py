138ab263f276e25d641a4e33232892b84d90f914
Remiat Alexandre <134190489+aremiat@users.noreply.github.com>
2025-01-24 08:59:01 +0100

    Clean code

--- backtest.py
+++ backtest.py
@@ -2,60 +2,63 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tickers = ['MA', 'TSLA', 'CMG', 'RMS', 'NOVO-B.CO', 'CRM', 'PGHN.SW', 'REGN', 'V', 'WSP',
+TICKERS = ['MA', 'TSLA', 'CMG', 'RMS', 'NOVO-B.CO', 'CRM', 'PGHN.SW', 'REGN', 'V', 'WSP',
            'AHT', 'ASML', 'LII', 'ASM', 'INTU', 'WST', 'SNPS', 'TSCO', 'FI', 'MTD']
-weights = [0.076160, 0.069118, 0.058923, 0.057336, 0.056844, 0.052907, 0.050583, 0.048601,
+WEIGHTS = [0.076160, 0.069118, 0.058923, 0.057336, 0.056844, 0.052907, 0.050583, 0.048601,
            0.048058, 0.047396, 0.046568, 0.046477, 0.046106, 0.044993, 0.043754, 0.043250,
            0.041264, 0.041211, 0.040239, 0.040211]
 
-start_date = '2017-01-01'
-end_date = '2025-01-01'
-data = yf.download(tickers, start=start_date, end=end_date)['Close']
-returns = data.pct_change().dropna()
-portfolio_returns = (returns * weights).sum(axis=1)
-
-# Additional index for MSCI ACWI Low Carbon Target
-index_ticker = 'CRBN'  # Ticker for the MSCI ACWI Low Carbon Target Index
-index_data = yf.download(index_ticker, start=start_date, end=end_date)['Close']
-index_returns = index_data.pct_change().dropna()
-common_dates = portfolio_returns.index.intersection(index_returns.index)
-portfolio_returns = portfolio_returns.loc[common_dates]
-index_returns = index_returns.loc[common_dates]
-combined_returns = pd.concat([
-    portfolio_returns,
-    index_returns
-], axis=1)
-
-combined_returns.columns = ['SHAP 20', 'MSCI ACWI Low Carbon Target']
-combined_cumulative_returns = (1 + combined_returns).cumprod()
-plt.figure(figsize=(10, 6))
-plt.plot(combined_cumulative_returns['SHAP 20'], label="SHAP 20 Cumulative Return", color='blue')
-plt.plot(combined_cumulative_returns['MSCI ACWI Low Carbon Target'], label="MSCI ACWI Low Carbon Target Cumulative Return", color='green')
-plt.title("SHAP 20 vs. MSCI ACWI Low Carbon Target Index: Cumulative Returns (2017-2025)")
-plt.xlabel("Year")
-plt.ylabel("Cumulative Return")
-plt.legend()
-plt.grid(True)
-plt.show()
-
-# Calculate the annualized returns (CAGR) for both the portfolio and the MSCI ACWI Low Carbon Target Index
-years = (common_dates[-1] - common_dates[0]).days / 365.25
-portfolio_annualized_return = (combined_cumulative_returns['SHAP 20'][-1]) ** (1/years) - 1
-index_annualized_return = (combined_cumulative_returns['MSCI ACWI Low Carbon Target'][-1]) ** (1/years) - 1
-
-print(f"Annualized Return (CAGR) from 2017 to 2025 for SHAP 20: {portfolio_annualized_return:.2%}")
-print(f"Annualized Return (CAGR) from 2017 to 2025 for MSCI ACWI Low Carbon Target Index: {index_annualized_return:.2%}")
+START_DATE = '2017-01-01'
+END_DATE = '2025-01-01'
 
 
-portfolio_cumulative_returns = (1 + portfolio_returns).cumprod()
-index_cumulative_returns = (1 + index_returns).cumprod()
-portfolio_annualized_returns = portfolio_cumulative_returns.resample('Y').last().pct_change().add(1)
-index_annualized_returns = index_cumulative_returns.resample('Y').last().pct_change().add(1)
-annualized_returns_per_year = pd.concat({
-    'SHAP 20 Annualized Return': portfolio_annualized_returns,
-    'MSCI ACWI Low Carbon Target Annualized Return': index_annualized_returns
-}, axis=1).dropna()
+if __name__ == '__main__':
+    data = yf.download(TICKERS, start=START_DATE, end=END_DATE)['Close']
+    returns = data.pct_change().dropna()
+    portfolio_returns = (returns * WEIGHTS).sum(axis=1)
 
-annualized_returns_per_year = (annualized_returns_per_year -1) * 100
-annualized_returns_per_year.columns = ['SHAP 20', 'MSCI ACWI Low Carbon Target']
-print(annualized_returns_per_year)
+    # Additional index for MSCI ACWI Low Carbon Target
+    index_ticker = 'CRBN'  # Ticker for the MSCI ACWI Low Carbon Target Index
+    index_data = yf.download(index_ticker, start=START_DATE, end=END_DATE)['Close']
+    index_returns = index_data.pct_change().dropna()
+    common_dates = portfolio_returns.index.intersection(index_returns.index)
+    portfolio_returns = portfolio_returns.loc[common_dates]
+    index_returns = index_returns.loc[common_dates]
+    combined_returns = pd.concat([
+        portfolio_returns,
+        index_returns
+    ], axis=1)
+
+    combined_returns.columns = ['SHPA 20', 'MSCI ACWI Low Carbon Target']
+    combined_cumulative_returns = (1 + combined_returns).cumprod()
+    plt.figure(figsize=(10, 6))
+    plt.plot(combined_cumulative_returns['SHPA 20'], label="SHPA 20 Cumulative Return", color='blue')
+    plt.plot(combined_cumulative_returns['MSCI ACWI Low Carbon Target'], label="MSCI ACWI Low Carbon Target Cumulative Return", color='green')
+    plt.title("SHPA 20 vs. MSCI ACWI Low Carbon Target Index: Cumulative Returns (2017-2025)")
+    plt.xlabel("Year")
+    plt.ylabel("Cumulative Return")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+    # Calculate the annualized returns (CAGR) for both the portfolio and the MSCI ACWI Low Carbon Target Index
+    years = (common_dates[-1] - common_dates[0]).days / 365.25
+    portfolio_annualized_return = (combined_cumulative_returns['SHPA 20'][-1]) ** (1/years) - 1
+    index_annualized_return = (combined_cumulative_returns['MSCI ACWI Low Carbon Target'][-1]) ** (1/years) - 1
+
+    print(f"Annualized Return (CAGR) from 2017 to 2025 for SHPA 20: {portfolio_annualized_return:.2%}")
+    print(f"Annualized Return (CAGR) from 2017 to 2025 for MSCI ACWI Low Carbon Target Index: {index_annualized_return:.2%}")
+
+
+    portfolio_cumulative_returns = (1 + portfolio_returns).cumprod()
+    index_cumulative_returns = (1 + index_returns).cumprod()
+    portfolio_annualized_returns = portfolio_cumulative_returns.resample('Y').last().pct_change().add(1)
+    index_annualized_returns = index_cumulative_returns.resample('Y').last().pct_change().add(1)
+    annualized_returns_per_year = pd.concat({
+        'SHPA 20 Annualized Return': portfolio_annualized_returns,
+        'MSCI ACWI Low Carbon Target Annualized Return': index_annualized_returns
+    }, axis=1).dropna()
+
+    annualized_returns_per_year = (annualized_returns_per_year -1) * 100
+    annualized_returns_per_year.columns = ['SHPA 20', 'MSCI ACWI Low Carbon Target']
+    print(annualized_returns_per_year)
--- financial_filters.py
+++ financial_filters.py
@@ -1,48 +1,63 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv("all_data.csv", sep=";")
-df_filtered = data[data['ITR'] <= 2.5]
 
-df_filtered = df_filtered[df_filtered['SDG_07_NET_ALIGNMENT_SCORE'] >= 2]
-
-criteria = ['Annualized return 20Y', 'Sharpe 20Y']
-scaler = StandardScaler()
-
-# Normalisation des critères dans df_filtered
-df_filtered[criteria] = scaler.fit_transform(df_filtered[criteria])
-df_filtered['score'] = df_filtered[criteria].mean(axis=1)
-top_20_best_in_class = df_filtered.sort_values(by='score', ascending=False).head(20)
-itr_average = top_20_best_in_class['ITR'].mean()
-
-while itr_average >= 2:
-    eligible_assets = df_filtered[df_filtered['ITR'] < 2]
-    new_top_20_best_in_class = eligible_assets.sort_values(by='score', ascending=False).head(20)
-    itr_average = new_top_20_best_in_class['ITR'].mean()
-    if itr_average < 2:
-        top_20_best_in_class = new_top_20_best_in_class
+NUM_TEMP = 2.5
+SDG_07_NET_ALIGNMENT_SCORE = 2
+ITR_THRESHOLD = 2.4
 
 
-print("Top 20 Best in Class avec ITR < 2 :")
-print(top_20_best_in_class)
-print(f"Moyenne ITR : {itr_average}")
+if __name__ == '__main__':
+    data = pd.read_csv("all_data.csv", sep=";")
+    df_filtered = data[data['ITR'] <= NUM_TEMP]
 
-total_score = top_20_best_in_class['score'].sum()
-top_20_best_in_class['weight'] = top_20_best_in_class['score'] / total_score
-print(top_20_best_in_class[['ticker', 'name', 'score', 'weight']])
+    df_filtered = df_filtered[df_filtered['SDG_07_NET_ALIGNMENT_SCORE'] >= SDG_07_NET_ALIGNMENT_SCORE]
 
-itr_global = (top_20_best_in_class['ITR'] * top_20_best_in_class['weight']).sum()
+    criteria = ['Annualized return 20Y', 'Sharpe 20Y']
+    scaler = StandardScaler()
 
-print("ITR global recalculé : ", itr_global)
-print(top_20_best_in_class[['ticker', 'Annualized return 20Y','Sharpe 20Y','score', 'weight']])
+    # Normalisation des critères dans df_filtered
+    df_filtered[criteria] = scaler.fit_transform(df_filtered[criteria])
+    df_filtered['score'] = df_filtered[criteria].mean(axis=1)
+    top_20_best_in_class = df_filtered.sort_values(by='score', ascending=False).head(20)
+    itr_average = top_20_best_in_class['ITR'].mean()
 
-if itr_global > 2:
-    normalisation_factor = 2 / itr_global
-    top_20_best_in_class['weight'] = top_20_best_in_class['weight'] * normalisation_factor
-    itr_global_normalised = (top_20_best_in_class['ITR'] * top_20_best_in_class['weight']).sum()
-    print("ITR global après normalisation : ", itr_global_normalised)
-else:
-    print("L'ITR global est déjà inférieur ou égal à 2.")
+    while itr_average >= 2:
+        eligible_assets = df_filtered[df_filtered['ITR'] < ITR_THRESHOLD]
+        new_top_20_best_in_class = eligible_assets.sort_values(by='score', ascending=False).head(20)
+        itr_average = new_top_20_best_in_class['ITR'].mean()
+        if itr_average < 2:
+            top_20_best_in_class = new_top_20_best_in_class
+        else:
+            ITR_THRESHOLD -= 0.1
 
-final_df = top_20_best_in_class[['ticker', 'name','Annualized return 20Y','Sharpe 20Y', 'score', 'weight']]
-final_df.to_csv("fund_composition.csv", sep=";", index=False)
+
+    print("Top 20 Best in Class avec ITR < 2 :")
+    print(top_20_best_in_class)
+    print(f"Moyenne ITR : {itr_average}")
+
+    total_score = top_20_best_in_class['score'].sum()
+    top_20_best_in_class['weight'] = top_20_best_in_class['score'] / total_score
+    print(top_20_best_in_class[['ticker', 'name', 'score', 'weight']])
+
+    itr_global = (top_20_best_in_class['ITR'] * top_20_best_in_class['weight']).sum()
+
+    print("ITR global recalculé : ", itr_global)
+    print(top_20_best_in_class[['ticker', 'Annualized return 20Y','Sharpe 20Y','score', 'weight']])
+
+    if itr_global > 2:
+        normalisation_factor = 2 / itr_global
+        top_20_best_in_class['weight'] = top_20_best_in_class['weight'] * normalisation_factor
+        itr_global_normalised = (top_20_best_in_class['ITR'] * top_20_best_in_class['weight']).sum()
+        print("ITR global après normalisation : ", itr_global_normalised)
+    else:
+        print("L'ITR global est déjà inférieur ou égal à 2.")
+
+    weight_sum = top_20_best_in_class['weight'].sum()
+    if abs(weight_sum - 1) > 1e-6:
+        print("Attention : La somme des poids n'est pas égale à 1, elle vaut :", weight_sum)
+    else:
+        print("Les poids sont correctement normalisés.")
+
+    final_df = top_20_best_in_class[['ticker', 'name','Annualized return 20Y','Sharpe 20Y', 'score', 'weight']]
+    final_df.to_csv("fund_composition.csv", sep=";", index=False)
